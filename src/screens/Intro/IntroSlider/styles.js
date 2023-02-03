4cf9afaa49c7a239c3c8def69921f29c5591d48b
EsraaRedaElgiz <110185098+EsraaRedaElgiz@users.noreply.github.com>
2023-02-04 01:59:40 +0200

    introSlider

--- src/screens/Intro/IntroSlider/styles.js
+++ src/screens/Intro/IntroSlider/styles.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
     }, imageStyle: {
         width: '100%',
-        height: '50%',
+        height: '60%',
         resizeMode:'contain'
     }
 
